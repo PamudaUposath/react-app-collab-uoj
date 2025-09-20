8e7aaa496a4eec3f4fb060ef18208542fe73aa2f
Lakdinu <158579473+BLakdinu@users.noreply.github.com>
2025-09-20 13:00:27 +0530

    updated the App.js file, from lakdinu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
-// This is a simple JavaScript file
+// Conflict Solving
 console.log('Hello from Pamuda');
-console.log('Solve the conflict');
+console.log('Hello from Lakdinu, a collaboator 1');
+
